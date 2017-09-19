5be89074b20f3cd2a2b0799c9126f406f4f82c0d
Ganeshkumar Ramachandran <gramachandran@Ganeshkumars-MacBook-Pro.local>
2017-09-20 01:52:44 +0530

    db changes

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const mongoDbConnector = require('./dbutils/mongo-connector');
 var index = require('./routes/index');
-
 var currentprice = require('./routes/currentprice');
 var app = express();
 
+mongoDbConnector.openDBConnetion();//open with default local host
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+
+
+if(mongoDbConnector.getInstance())
+    console.log("db connection completed");
+else
+    console.log("db connection not available");
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -23,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static('public'))
 app.use('/', index);
-app.use('/currentprice',currentprice);
+app.use('/currentprice',currentprice.router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +44,7 @@ app.use(function(req, res, next) {
 //   // set locals, only providing error in development
 //   res.locals.message = err.message;
 //   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+//
 //   // render the error page
 //   res.status(err.status || 500);
 //   res.render('error');
